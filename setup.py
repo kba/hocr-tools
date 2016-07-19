0a82abf0d2653c58998f410dc22f169aa1e349f5
Konstantin Baierer <unixprog@gmail.com>
2016-07-19 03:09:48 +0200

    Moved get_prop,get_text,get_bbox to a common lib dir

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import glob
 from setuptools import setup
 setup(
     name = "hocr_tools",
-    version = "0.2",
+    version = "0.1.1",
     author = 'Thomas Breuel',
     description = 'Advanced tools for hOCR integration',
     packages = ['hocrlib'],
